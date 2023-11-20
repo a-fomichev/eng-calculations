326e792a9408e4529d005afd44df8d0c8b02ceb7
a-fomichev <151375520+a-fomichev@users.noreply.github.com>
2023-11-20 07:40:31 +0300

    Delete passenger_wsgi.py

--- passenger_wsgi.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys, os
-sys.path.append('/home/d/deniatest/flask/HelloFlask/') # указываем директорию с проектом
-sys.path.append('/home/d/deniatest/.local/lib/python3.6/site-packages') # указываем директорию с библиотеками, куда поставили Flask
-from HelloFlask import app as application # когда Flask стартует, он ищет application. Если не указать 'as application', сайт не заработает
-from werkzeug.debug import DebuggedApplication # Опционально: подключение модуля отладки
-application.wsgi_app = DebuggedApplication(application.wsgi_app, True) # Опционально: включение модуля отадки
-application.debug = False  # Опционально: True/False устанавливается по необходимости в отладке
